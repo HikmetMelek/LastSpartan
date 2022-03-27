d1ff4407a41c00a3db5ed535da4d32a752ca8fb0
Melek <hmlkshn@gmail.com>
2022-03-27 21:18:11 +0300

    spartan CRUD completed

--- src/test/java/com/cydeo/stepDefs/SpartanFlowSteps.java
+++ src/test/java/com/cydeo/stepDefs/SpartanFlowSteps.java
@@ -2,9 +2,14 @@ package com.cydeo.stepDefs;
 
 import com.cydeo.pages.pojo.SpartanCreated;
 import com.cydeo.utilities.ConfigurationReader;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 
 import static io.restassured.RestAssured.*;
 
@@ -12,6 +17,8 @@ public class SpartanFlowSteps {
 
     Response mockSpartanJson;
     Response postResponse;
+    Response getResponse;
+    SpartanCreated mySpartan;
 
     @When("User sends a request to Mock API for a mock Spartan Data")
     public void userSendsARequestToMockAPIForAMockSpartanData() {
@@ -21,7 +28,9 @@ public class SpartanFlowSteps {
                 .header("X-API-Key",67878690)
                 .when().get();
 
-        mockSpartanJson.prettyPrint();
+    //    mockSpartanJson.prettyPrint();
+
+        Assert.assertEquals(200,mockSpartanJson.statusCode());
 
     }
 
@@ -30,7 +39,7 @@ public class SpartanFlowSteps {
     public void userUsesMockDataToCreateASpartan() {
         baseURI= ConfigurationReader.get("spartan.apiUrl");
 
-        SpartanCreated mySpartan= new SpartanCreated();
+        mySpartan= new SpartanCreated();
         mySpartan.setName(mockSpartanJson.path("name"));
         mySpartan.setGender(mockSpartanJson.path("gender"));
         Long phone= Long.valueOf(mockSpartanJson.path("phone").toString());
@@ -42,7 +51,21 @@ public class SpartanFlowSteps {
                 .body(mySpartan)
                 .when().post("api/spartans");
 
-        postResponse.prettyPrint();
+        Assert.assertEquals(201,postResponse.statusCode());
+        //        postResponse.prettyPrint();
+              /*
+        {
+    "success": "A Spartan is Born!",
+    "data": {
+        "id": 174,
+        "name": "Heddi",
+        "gender": "Female",
+        "phone": 2282413422
+    }
+}
+         */
+
+
 
     }
 
@@ -54,12 +77,68 @@ public class SpartanFlowSteps {
             id= postResponse.path("data.id");
         }
 
-        Response response = given().accept(ContentType.JSON)
+        getResponse = given().accept(ContentType.JSON)
                     .and().pathParam("id", id)
                     .when().get("/api/spartans/{id}");
 
-        response.prettyPrint();
+    //    getResponse.prettyPrint();
+        Assert.assertEquals(200,getResponse.statusCode());
+        /*
+        {
+    "id": 174,
+    "name": "Heddi",
+    "gender": "Female",
+    "phone": 2282413422
+}
+         */
 
+    }
+
+    @Then("created Spartan has same info with post request")
+    public void createdSpartanHasSameInfoWithPostRequest() {
+        String expectedName= postResponse.path("data.name");
+        String actualName= getResponse.path("name");
+        Assert.assertEquals(expectedName,actualName);
+
+    }
+
+    @And("User update all the fields of created Spartan")
+    public void userUpdateAllTheFieldsOfCreatedSpartan() {
+        baseURI= ConfigurationReader.get("spartan.apiUrl"); // do not forget
+
+        mySpartan.setName("Jessica");
+        mySpartan.setGender("Female");
+        mySpartan.setPhone(55545685215l);
+
+        Response putResponse= given()
+                .contentType(ContentType.JSON)
+                .and()
+                .pathParam("id", postResponse.path("data.id"))
+                .body(mySpartan)
+                .when().put("/api/spartans/{id}");
+
+        Assert.assertEquals(204, putResponse.statusCode());
+        Assert.assertEquals("", putResponse.body().asString());
+
+    }
+
+    @Then("User deletes spartan {int}")
+    public void userDeletesSpartan(int id) {
+        baseURI= ConfigurationReader.get("spartan.apiUrl"); // do not forget
+        if(id==0){
+            id=postResponse.path("data.id");
+        }
+
+        Response deleteResponse= given().pathParam("id",id)
+                .when().delete("/api/spartans/{id}");
+
+        Assert.assertEquals(204,deleteResponse.statusCode());
+
+        given().accept(ContentType.JSON)
+                .pathParam("id",id)
+                .when().get("/api/spartans/{id}")
+                .then().statusCode(404)
+                .body("error", Matchers.equalToIgnoringCase("Not Found"));
     }
 
 
--- src/test/resources/features/SpartanFlow.feature
+++ src/test/resources/features/SpartanFlow.feature
@@ -3,4 +3,8 @@ Feature: Test Spartan API with complete CRUD operations
   Scenario: Read data from Spartan API
     Given User sends a request to Mock API for a mock Spartan Data
     When User uses Mock Data to create a Spartan
-    When User send a request to Spartan API with ID 0
+    And User send a request to Spartan API with ID 0
+    Then created Spartan has same info with post request
+    And User update all the fields of created Spartan
+    Then User deletes spartan 0
+
