aa6a914c8cc275f7f654867463b908710343cfd1
Melek <hmlkshn@gmail.com>
2022-03-27 18:50:24 +0300

    initial

---
+++ src/test/java/com/cydeo/utilities/ConfigurationReader.java
@@ -0,0 +1,28 @@
+package com.cydeo.utilities;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigurationReader {
+    private static Properties properties;
+
+    static {
+
+        try {
+            String path="configuration.properties";
+            FileInputStream fileInput= new FileInputStream(path);
+            properties=new Properties();
+            properties.load(fileInput);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String get(String key){
+        return properties.getProperty(key);
+    }
+
+
+}
